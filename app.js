7ac549da9d0e098cb07a4ffc7a73b0b17989aa2b
MilanBrkic <milan.brkic1998@gmail.com>
2020-02-03 13:23:13 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const app = new Vue({
   el: '#app',
   data: {
@@ -12,16 +13,70 @@ const app = new Vue({
         done: false
       });
       this.newTodo = '';
-      console.log(this.todos);
+      updateJson();
+      addListeners();
     },
     removeTodo(todo) {
       const todoIndex = this.todos.indexOf(todo);
       this.todos.splice(todoIndex, 1);
+      updateJson();
+      addListeners();
     },
     allDone() {
       this.todos.forEach(todo => {
         todo.done = true;
-      });
+    });
+    updateJson();
     }
   }
 });
+
+
+updateJs();
+
+function updateJson(){
+  var niz = new Array();
+  for(var i=0;i<app.todos.length;i++){
+    var pom = new Object();
+    pom.title = app.todos[i].title;
+    pom.done = app.todos[i].done;
+    niz.push(pom);
+  }
+  
+  var myJson = JSON.stringify(niz);
+  
+  window.localStorage.setItem('json', myJson);
+  
+}
+
+function updateJs(){
+  var niz = new Array();
+  niz=JSON.parse(window.localStorage.getItem('json'));
+  for(var i =0;i<niz.length;i++){
+    app.todos.push({
+      title: niz[i].title,
+      done: niz[i].done
+    });
+  }
+
+}
+
+
+
+  window.addEventListener('load',function(){
+    addListeners();    
+  })
+
+  var broj=0;
+
+function addListeners(){
+    var checkboxes = document.getElementsByClassName('checked');
+    for(var i =0;i<checkboxes.length;i++){
+      if(checkboxes[i]==null){
+        return;
+      }
+      checkboxes[i].addEventListener('change', updateJson)
+    }
+}
+
+
